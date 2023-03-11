8a66aa92d6096f0a05c9296506aa256d1d636766
Mukul <mukulkashyapk@gmail.com>
2023-03-11 13:17:37 +0530

    go router and riverpod initilised

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,39 @@
+import 'package:attendence_system/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final GoRouter router = ref.watch(routerProvider);
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      routeInformationParser: router.routeInformationParser,
+      routerDelegate: router.routerDelegate,
+      routeInformationProvider: router.routeInformationProvider,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        drawer: Drawer(),
-        appBar: AppBar(
-          title: Text('Testing'),
-          backgroundColor: Colors.deepPurple,
-        ),
-        body: const Center(
-          child: Text('Hello World!'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: const Center(
+        child: Text("Home page ha Bhai dekh lo"),
       ),
     );
   }
---
+++ lib/router.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+import 'main.dart';
+
+// This is the router provider that will be used in the main.dart file
+// to pass the router to the MaterialApp.router
+final routerProvider = Provider<GoRouter>((ref) {
+  return GoRouter(
+      //if page not found then it will show the page not found from here.
+      errorBuilder: (context, state) => const Center(
+            child: Text("Page Not Found"),
+          ),
+      routes: [
+        //initializing the home page
+        GoRoute(path: '/', builder: (context, state) => const HomePage()),
+      ]);
+});
