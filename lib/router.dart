1cd53b7973313a1bff63e4b61a0f7fddc2a396ae
mohitR <Officialu369@gmail.com>
2023-03-17 20:05:25 +0530

    [structured]

--- lib/router.dart
+++ lib/router.dart
@@ -2,8 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-
-import 'main.dart';
+import 'home.dart';
 
 // This is the router provider that will be used in the main.dart file
 // to pass the router to the MaterialApp.router
